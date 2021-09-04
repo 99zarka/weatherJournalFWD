77a6e30cfb02ac40a381c1ffb534d5fa5a31d44b
99zarka <53001011+99zarka@users.noreply.github.com>
2021-09-04 07:41:22 +0200

    Server-Side setup
    1- Setup the web application and Express Environment.
    2- Projecting Dependencies (Express, body-parser & cors).
    3- Setup Local Server.
    4- Add a GET route that returns the "projectData" object in the server code.
    5- Add a POST route that adds incoming data to projectData.

--- server.js
+++ server.js
@@ -2,18 +2,37 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 6969;
+const server=app.listen(port,function(){console.log("server is running on port",port)});
+
+app.get('/forecast',function(req,res){
+    res.send(projectData);
+})
+
+app.post('/addNewData',function(req,res){
+    projectData['temperature']=req.body.temperature;
+    projectData['date']=req.body.date;
+    projectData['userResponse']=req.body.userResponse;
+    //app.listen(port,function(){console.log(9,projectData)});
+    console.log(projectData);
+    res.end("{}");
+})
